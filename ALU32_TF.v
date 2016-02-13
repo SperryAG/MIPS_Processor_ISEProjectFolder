d3bbf8c6c225a2cd066cbad98cc074eea7968a08
SperryAG <agsperry21@gmail.com>
2016-02-12 21:18:27 -0800

    ALU32_TF.v fixed and simulates
    I did not check that the alu operations work correctly though

---
+++ ALU32_TF.v
@@ -0,0 +1,95 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   21:13:00 02/12/2016
+// Design Name:   ALU_32BIT
+// Module Name:   C:/Users/Andrew/Documents/ISEWorkspace/testgate/MIPS_Processor_ISEProjectFolder/ALU32_TF.v
+// Project Name:  testgate
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: ALU_32BIT
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module ALU32_TF;
+
+	// Inputs
+	reg [5:0] Func_in;
+	reg [31:0] A_in;
+	reg [31:0] B_in;
+
+	// Outputs
+	wire [31:0] O_out;
+	wire Branch_out;
+
+	// Instantiate the Unit Under Test (UUT)
+	ALU_32BIT uut (
+		.Func_in(Func_in), 
+		.A_in(A_in), 
+		.B_in(B_in), 
+		.O_out(O_out), 
+		.Branch_out(Branch_out)
+	);
+
+	initial begin
+		// Initialize Inputs
+		Func_in = 0;
+		A_in = 0;
+		B_in = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+		Func_in = 6'b111000; //BRANCH LESS THAN ZERO
+		A_in = 32'b11111111111111111111111111111111;
+		B_in = 32'b11111111111111111111111111111111;
+		//EXPECT O_out = A_in;
+		//EXPECT Branch_out = 1;
+		#100;
+		Func_in = 6'b111001; //BRANCH GREATER THAN OR EQUAL TO ZERO
+		A_in = 32'b00000000000000000000000000000000;
+		B_in = 32'b00000000000000000000000000000000;
+		
+		//EXPECT O_out = A_in;
+		//EXPECT Branch_out = 1;
+		#100;
+		Func_in = 6'b111100; //BRANCH EQUAL 
+		A_in = 32'b00000000000000000000000000000000;
+		B_in = 32'b00000000000000000000000000000001;
+		//EXPECT Branc_out = 0;
+		#100;
+		Func_in = 6'b111101; //BRANCH NOT EQUAL
+		A_in = 32'b00000000000000000000000000000000;
+		B_in = 32'b00000000000000000000000000000001;
+		//EXPECT Branc_out = 1;
+		#100;
+		Func_in = 6'b111110; //BRANCH LESS THAN OR EQUAL TO ZERO
+		A_in = 32'b00000000000000000000000000000000;
+		B_in = 32'b00000000000000000000000000000000;
+		
+		//EXPECT Branc_out = 1;
+		#100;
+		Func_in = 6'b111111; //BRANCH GREATER THAN ZERO
+		A_in = 32'b00000000000000000000000000001111;
+		B_in = 32'b00000000000000000000000000000000;
+		
+		//EXPECT Branc_out = 1;
+		#100;
+		
+		
+        
+		// Add stimulus here
+		$finish;
+	end
+      
+endmodule
--- isim/PROCESSOR_32Bit_tb_isim_beh.exe.sim/work/PROCESSOR_32Bit_tb_isim_beh.exe_main.c
+++ isim/PROCESSOR_32Bit_tb_isim_beh.exe.sim/work/PROCESSOR_32Bit_tb_isim_beh.exe_main.c
@@ -21,6 +21,7 @@ char *STD_TEXTIO;
 char *IEEE_P_3499444699;
 char *IEEE_P_3620187407;
 char *WORK_P_3440277793;
+char *IEEE_P_0774719531;
 
 
 int main(int argc, char **argv)
@@ -35,6 +36,7 @@ int main(int argc, char **argv)
     ieee_p_3620187407_init();
     std_textio_init();
     ieee_p_1242562249_init();
+    ieee_p_0774719531_init();
     work_a_2110464610_3212880686_init();
     work_a_3166362984_3212880686_init();
     work_a_1879590719_3212880686_init();
@@ -66,6 +68,7 @@ int main(int argc, char **argv)
     IEEE_P_3499444699 = xsi_get_engine_memory("ieee_p_3499444699");
     IEEE_P_3620187407 = xsi_get_engine_memory("ieee_p_3620187407");
     WORK_P_3440277793 = xsi_get_engine_memory("work_p_3440277793");
+    IEEE_P_0774719531 = xsi_get_engine_memory("ieee_p_0774719531");
 
     return xsi_run_simulation(argc, argv);
 
