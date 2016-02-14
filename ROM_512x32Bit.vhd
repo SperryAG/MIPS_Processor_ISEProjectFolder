e8be6054449744fab758ca68501ca00fbaa78118
David Pynes <Dpynes@gmail.com>
2016-02-14 14:52:08 -0800

    Update on ROM  and put back instruction in memfile

--- ROM_512x32Bit.vhd
+++ ROM_512x32Bit.vhd
@@ -29,7 +29,7 @@ begin
 		variable L: line;
 		variable ch: character;
 		variable i, index, result: integer;
-		type ramtype is array (511 downto 0) of STD_LOGIC_VECTOR(31 downto 0);
+		type ramtype is array (511 downto 0) of STD_LOGIC_VECTOR(7 downto 0);
 		variable mem: ramtype;
 	begin
 		-- initialize memory from file
@@ -52,13 +52,32 @@ begin
 			else report "Format error on line" & integer'
 				image(index) severity error;
 			end if;
-			mem(index)(35-i*4 downto 32-i*4) := std_logic_vector(to_unsigned(result,4));
+			if i = 1 then
+				mem(index+3)(7 downto 4) := std_logic_vector(to_unsigned(result,4));
+			elsif i = 2 then
+				mem(index+3)(3 downto 0) := std_logic_vector(to_unsigned(result,4));
+			elsif i = 3 then
+				mem(index+2)(7 downto 4) := std_logic_vector(to_unsigned(result,4));
+			elsif i = 4 then
+				mem(index+2)(3 downto 0) := std_logic_vector(to_unsigned(result,4));
+			elsif i = 5 then
+				mem(index+1)(7 downto 4) := std_logic_vector(to_unsigned(result,4));
+			elsif i = 6 then
+				mem(index+1)(3 downto 0) := std_logic_vector(to_unsigned(result,4));
+			elsif i = 7 then
+				mem(index)(7 downto 4) := std_logic_vector(to_unsigned(result,4));
+			elsif i = 8 then
+				mem(index)(3 downto 0) := std_logic_vector(to_unsigned(result,4));
+			end if;
 		end loop;
-			index := index + 1;
+			index := index + 4;
 		end loop;
 		-- read memory
 		loop
-			dataIO <= mem(to_integer(unsigned(addr(8 DOWNTO 0))));
+			dataIO(7 DOWNTO 0)   <= mem(to_integer(unsigned(addr(8 DOWNTO 0))));
+			dataIO(15 DOWNTO 8)  <= mem(to_integer(unsigned(addr(8 DOWNTO 0)))+1);
+			dataIO(23 DOWNTO 16) <= mem(to_integer(unsigned(addr(8 DOWNTO 0)))+2);
+			dataIO(31 DOWNTO 24) <= mem(to_integer(unsigned(addr(8 DOWNTO 0)))+3);
 			wait on addr;
 		end loop;
 	end process;
